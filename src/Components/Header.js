47efb4396c2b1580ebb44c56b6061f561813e114
GiaFil7 <GiaFil007@gmail.com>
2020-09-21 01:49:07 +0300

    Fix meta data and add logos

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Header () {
   return (
     <div className='header'>
+      <img className='logo' src='tree of life.png' alt='logo' />
       <h1>Growth Book</h1>
     </div>
   )
