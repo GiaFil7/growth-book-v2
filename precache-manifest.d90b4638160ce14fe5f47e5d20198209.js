8688bfe3d7965b10b1ca246d330d1b4b8fec6430
GiaFil7 <GiaFil007@gmail.com>
2020-10-02 00:34:56 +0300

    Updates

---
+++ precache-manifest.d90b4638160ce14fe5f47e5d20198209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6156c640e72fae23fa29cb6701c567",
+    "url": "/growth-book-v2/index.html"
+  },
+  {
+    "revision": "667ce03b5dbff46b20fe",
+    "url": "/growth-book-v2/static/css/main.5694be86.chunk.css"
+  },
+  {
+    "revision": "90ceccba43b17d3120a9",
+    "url": "/growth-book-v2/static/js/2.a9e750e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/growth-book-v2/static/js/2.a9e750e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667ce03b5dbff46b20fe",
+    "url": "/growth-book-v2/static/js/main.77dccba4.chunk.js"
+  },
+  {
+    "revision": "d1fa82c2219604c69c10",
+    "url": "/growth-book-v2/static/js/runtime-main.26782556.js"
+  }
+]);
